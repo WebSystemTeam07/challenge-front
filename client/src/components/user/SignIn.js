af5c916272dd0a4b07b77f1622d6eea31cc3ff72
lnalice <80760544+lnalice@users.noreply.github.com>
2022-11-26 22:00:24 +0900

    update : sign_in/sign_up - 수정 후 디자인

--- client/src/components/user/SignIn.js
+++ client/src/components/user/SignIn.js
@@ -1,6 +1,7 @@
 import { React, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './../../css/sign.css'
+import logo from './../../assets/FOFE_임시로고.png'
 
 export default function SignIn() {
     const navigate = useNavigate();
@@ -8,32 +9,18 @@ export default function SignIn() {
 
     return (
         <container id='container'>
-            <div id='signContainer'>
-                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKChhWEeW9pvQsnQvtPCW65MveZmIOKw-kaA&usqp=CAU'/>
-                <div class='title'>로그인해서 챌린지 이어하기</div>
-                {!view && <div className='subTitle'>
-                    이메일을 입력해주세요
-                </div>}
-                {view && <div className='subTitle'>
-                    비밀번호를 입력해주세요
-                </div>}
-                {!view && <><div id = 'horizon'>
-                    <input className='oneInputBlock' type='email' id='email' name='email' placeholder='이메일 (예시 : challenge@ajou.ac.kr)' />
-                    <button className='goButton' onClick = {() => {
-                        setView(true)
-                    }}>next</button>
+                <img src={logo} style={{width : "200px"}}/>
+                <div id='title'>로그인하고 챌린지 이어하기</div>
+                <div className='bigEnter'/>
+                <div id = 'inputContainer'>
+                    <input className='inputBlock' type='email' id='email' name='email' placeholder='이메일 (예시 : challenge@gmail.com)' />
+                    <input className='inputBlock' type='password' id='password' name='password' placeholder='비밀번호' />
                 </div>
-                <hr style={{ width: "30%" }} />
-                <a onClick={() => navigate("/signup")} >ID 생성하기</a></>}
-                {
-                    view && <div id = 'horizon'>
-                    <input className='oneInputBlock' type='password' id='password' name='password' placeholder='비밀번호' />
-                    <button className='goButton' onClick = {() => {
-                        setView(true)
-                    }}>go!</button>
-                </div>
-                }
-            </div>
+                    <div className='bigEnter'/>
+                    <button className='signButton' onClick = {() => {
+                        navigate("/")
+                    }}>로그인</button>
+                <a onClick={() => navigate("/signup")} >회원가입하기</a>
         </container>
     )
 }
--- client/src/components/user/SignUp.js
+++ client/src/components/user/SignUp.js
@@ -1,37 +1,43 @@
-import { React } from 'react'
+import { React, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './../../css/sign.css'
-// https://www.icloud.com/
+import logo from './../../assets/FOFE_임시로고.png'
+
 export default function SignIn() {
     const navigate = useNavigate();
+    const [next, setNext] = useState(false);
+
+    // TO DO : auth인증되지 않았다면 다음버튼 disable
 
     return (
-        // <container id='container'>
-        //     <div id='signContainer'>
-        //         <div class='title'>로그인</div>
-        //         <div class='subTitle'>이메일과 비밀번호를 입력하여 로그인해주세요</div>
-        //         {/* <form> */}
-        //             {/* <div> */}
-        //             <input className='inputBlock' type='email' id='email' name='email' placeholder='이메일을 입력하세요 (예시 : challenge@ajou.ac.kr)' />
-        //             {/* </div> */}
-        //             {/* <div> */}
-        //             <input className='inputBlock' type='password' id='password' name='password' placeholder='비밀번호 입력하세요' />
-        //             {/* </div> */}
-        //             {/* <a>회원이 아니신가요? 30일 챌린지를 진행해보세요</a> */}
-        //         {/* </form> */}
-        //             <button class='signButton' >login</button>
-        //     </div>
-        // </container>
-
         <container id='container'>
-            <div id='signContainer'>
-                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKChhWEeW9pvQsnQvtPCW65MveZmIOKw-kaA&usqp=CAU'/>
-                <div class='title'>챌린지를 시작해보세요</div>
-                    <input className='inputBlock' type='email' id='email' name='email' placeholder='이메일 (예시 : challenge@ajou.ac.kr)' />
-                    <input className='inputBlock' type='password' id='pasword' name='password' placeholder='비밀번호' />
-                    <hr style={{ width: "20%" }} />
-                    <button className='signButton'>회원가입</button>
-            </div>
+            <img src={logo} style={{ width: "200px" }} />
+            <div id='title'>챌린지를 시작해 보세요</div>
+            <div className='bigEnter' />
+            {!next && <div id='inputContainer'>
+                <div id='horizon'>
+                    <input className='inputBlock' type='email' id='inputBlock_email' name='email' placeholder='이메일 (예시 : challenge@gmail.com)' />
+                    <button className='authButton' onClick={() => {
+                    }}>인증</button>
+                </div>
+                <input className='inputBlock' type='Number' id='authNum' name='authNum' placeholder='인증번호' />
+            </div>}
+            {next &&
+                <div id='inputContainer'>
+                    <input className='inputBlock' type='email' id='email' name='email' placeholder='닉네임' />
+                    <input className='inputBlock' type='password' id='password' name='password' placeholder='비밀번호' />
+                </div>
+            }
+            <div className='bigEnter' />
+            
+            {!next &&<button className='signButton' onClick={() => {
+                setNext(true)
+            }}>다음</button>}
+            {next &&
+                <button className='signButton' onClick={() => {
+                    navigate("/")
+                }}>회원가입</button>
+            }
         </container>
     )
 }
