0f94044fb604113fb4abdb343f7a88c272d4b10a
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2022-12-04 13:17:58 +0900

    그룹챌린지 디테일 페이지 수정

--- src/App.js
+++ src/App.js
@@ -8,27 +8,13 @@ import MyPage from './components/myPage/MyPage'
 import SignIn from './components/user/SignIn';
 import SignUp from './components/user/SignUp';
 
+import Rounter from "./Router";
+
 function App() {
 
   return (
-    <div className="App">
-      <p>
-        <NavLink to='/'>Home&nbsp;</NavLink>
-      </p>
-      <NavLink to="/MyChallenge" style={{ textDecoration: 'none' }}>MyChallenge&nbsp;&nbsp;</NavLink>
-      <NavLink to="/GroupChallengePage" style={{ textDecoration: 'none' }}>GroupChallengePage&nbsp;&nbsp;</NavLink>
-      <NavLink to="/NewChallenge" style={{ textDecoration: 'none' }}>New Challenge&nbsp;&nbsp;</NavLink>
-      <Routes>
-        <Route path='/' />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/MyChallenge" element={<MyChallenge />} />
-        <Route path="/GroupChallengePage" element={<GroupChallengeMain />} />
-        <Route path="/groupChallengeMain/groupChallengeDetail" element={<GroupChallengeDetail/>}/>
-        <Route path="/NewChallenge" element={<NewChallenge />} />
-      </Routes>
-
+    <div>
+      <Rounter/>
     </div>
   );
 }
