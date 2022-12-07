10a5f04c642c8327b6e076683b6b254667622483
hanuuny <hanuuny@gmail.com>
2022-12-07 10:40:32 +0900

    add font

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,35 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Main from "./pages/Main"
-import Login from "./pages/Login"
-import Introduce from "./pages/Introduce"
-import GroupChallengeMain from "./pages/GroupChallengeMain"
-import GroupChallengeDetail from "./pages/GroupChallengeDetail"
+import MyChallenge from './pages/MyChallenge';
+import GroupChallengeMain from './pages/GroupChallengeMain';
+import NewChallenge from './components/NewChallenge';
+import GroupBoardDetail from './pages/GroupBoardDetail'
+import MyPage from './components/myPage/MyPage'
+import SignIn from './components/user/SignIn';
+import SignUp from './components/user/SignUp';
+
+import ChattingBoard from './components/GroupBoardComponent/ChattingBoard';
+
+import Main from './pages/Main';
+import Introduce from './pages/Introduce';
+import GroupBoard from './pages/GroupBoard'
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<Main />} />
-                <Route exact path="/login" element={<Login />} />
-                <Route exact path="/introduce" element={<Introduce />} />
-                <Route exact path="/groupChallengeMain" element={<GroupChallengeMain/>}/>
-                <Route exact path="/groupChallengeMain/groupChallengeDetail" element={<GroupChallengeDetail/>}/>
+            <Route path='/' element={<Main />}/>
+            <Route path='/introduce' element={<Introduce />}/>
+            <Route path="/mychallenge" element={<MyChallenge/>} />
+            <Route path="/groupchallengepage" element={<GroupChallengeMain/>}/>
+            <Route path="/newchallenge" element={<NewChallenge/>}/>
+            <Route path="/groupchallengepage/board" element={<GroupBoard/>}/>
+            <Route path="/login" element={<SignIn />}/>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/mypage" element={<MyPage/>} />
+            <Route path="/groupchallengepage/board/detail" element={<GroupBoardDetail />} />
+            <Route path="/chat" element={<ChattingBoard />} />
             </Routes>
         </BrowserRouter>
     );
