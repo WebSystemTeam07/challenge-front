24ce9d09fb07fe9daada8723b08e50fac049b1e2
lnalice <80760544+lnalice@users.noreply.github.com>
2022-12-11 17:36:47 +0900

    add : header, navigater - cookie handler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,37 @@
 import { Link } from "react-router-dom"
-
+import { useCookies } from "react-cookie";
 import styles from "../components/styles/component.module.scss"
+import { useEffect, useState } from "react";
+import { style } from "@mui/system";
 
-function Header({loginUrl}) {
-    return(
+function Header({ loginUrl }) {
+    const [cookies, setCookie, removeCookie] = useCookies(["userData"]);
+    const [login, setLogin] = useState(false);
+    useEffect(()=>{
+        console.log(login)
+        if(cookies.userData == undefined)
+        setLogin(false)
+        else
+        setLogin(true)
+    },[cookies])
+    return (
         <div className={styles.headerContainer}>
-            <Link to='/login' style={{ textDecoration: "none" }}>
-                <p>로그인</p>
-            </Link>
-            <Link to='/signup' style={{ textDecoration: "none" }}>
-                <p>회원가입</p>
-            </Link>
+            {!login &&
+                <>
+                    <Link to='/login' style={{ textDecoration: "none" }}>
+                        <p>로그인</p>
+                    </Link>
+                    <Link to='/signup' style={{ textDecoration: "none" }}>
+                        <p>회원가입</p>
+                    </Link>
+                </>
+            }
+            {
+                login &&
+                <p onClick={()=>{
+                    removeCookie("userData")
+                }} style={{cursor :"pointer"}}>로그아웃</p>
+            }
         </div>
     );
 }
