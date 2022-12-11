0f1aeba2f33b38a7ffd601549b7f319c891ca29f
hay0107 <hay0107@ajou.ac.kr>
2022-12-12 05:15:38 +0900

    btn error

--- src/components/myChallenge/CheckAuth.js
+++ src/components/myChallenge/CheckAuth.js
@@ -15,9 +15,10 @@ const CheckAuth=(props)=>{
     let startDate_arr=startDate.split("-");
     let stDate=new Date(startDate_arr[0],startDate_arr[1],startDate_arr[2]);
     let today=new Date(year,month,date);
+    
    console.log("date",today,stDate);
-    let day=(today.getTime()-stDate.getTime())/(1000*60*60*24);
-     const tmp_attend={"id":props.challengeId,"attend":'T',"userId":props.userId,"day":day+1}
+    let day=(today.getTime()-stDate.getTime())/(1000*60*60*24)+1;
+     const tmp_attend={"id":props.challengeId,"attend":'T',"userId":props.userId,"day":day}
     const req_attend={"challengeId":props.challengeId,"userId":props.userId}
      setAttend([...attend,tmp_attend]);
     axios.put(port.url+`/userTask/status/${day}`,req_attend)
